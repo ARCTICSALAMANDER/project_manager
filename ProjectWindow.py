2f5200131403b22e761b73c63ea372a6185b94b6
ARCTIC_SALAMANDER <ruslanau365@gmail.com>
2025-10-18 12:00:20 +0300

    Enchanced UI, made task adding to listWidget

--- ProjectWindow.py
+++ ProjectWindow.py
@@ -1,20 +1,25 @@
 import sys
 import string
 from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtWidgets import QMainWindow, QApplication, QVBoxLayout
 
 
 class ProjectWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, projectName):
         super().__init__()
         self.setupUi(self)
+        self.projectName = projectName
 
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
+        MainWindow.setObjectName("Project Manager")
         MainWindow.resize(800, 600)
 
         self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        self.centralwidget.setStyleSheet('''
+            background-color: rgb(26, 26, 26);
+            color: white;
+        ''')
 
         self.ConsoleSplitter = QtWidgets.QSplitter(parent=self.centralwidget)
         self.ConsoleSplitter.setGeometry(QtCore.QRect(530, 40, 256, 511))
@@ -45,9 +50,22 @@ class ProjectWindow(QMainWindow):
 
         self.treeView = QtWidgets.QTreeView(parent=self.treeCheckBoxCont)
         self.treeView.setObjectName("treeView")
+        self.treeView.setStyleSheet('''
+            background-color: rgb(15, 15, 15);
+            border: 1px solid white;
+            border-radius: 5px;
+            color: white;
+        ''')
 
         self.listWidget = QtWidgets.QListWidget(parent=self.treeCheckBoxCont)
         self.listWidget.setObjectName("listWidget")
+        self.listWidget.setStyleSheet('''
+            background-color: rgb(15, 15, 15);
+            border: 1px solid white;
+            border-radius: 5px;
+            color: white;
+        ''')
+        self.listWidget.setSpacing(2)
 
         self.splitter = QtWidgets.QSplitter(parent=self.centralwidget)
         self.splitter.setGeometry(QtCore.QRect(20, 10, 491, 21))
@@ -56,6 +74,13 @@ class ProjectWindow(QMainWindow):
 
         self.backButton = QtWidgets.QPushButton(parent=self.splitter)
         self.backButton.setObjectName("backButton")
+        self.backButton.setFixedWidth(70)
+        self.backButton.setStyleSheet('''
+            border: 1px solid white;
+            background-color: rgb(15, 15, 15);
+            border-radius: 3px;
+            padding: 2px;
+        ''')
 
         self.projectName = QtWidgets.QLabel(parent=self.splitter)
         self.projectName.setObjectName("projectName")
@@ -74,7 +99,7 @@ class ProjectWindow(QMainWindow):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Project Manager"))
         self.backButton.setText(_translate("MainWindow", "<- Назад"))
         self.projectName.setText(_translate("MainWindow", "TextLabel"))
 
@@ -96,9 +121,42 @@ class ProjectWindow(QMainWindow):
                 self.consoleInput.setText("> ")
             self.consoleInput.blockSignals(False) # восстанавливаем передачу сигналов
 
+    def add_task(self, taskName: str):
+        '''Добавление задачи в список задач'''
+        task = Task(taskName)
+        taskInList = QtWidgets.QListWidgetItem()
+        taskInList.setSizeHint(task.sizeHint())
+        self.listWidget.addItem(taskInList)
+        self.listWidget.setItemWidget(taskInList, task)
+
+
+class Task(QtWidgets.QWidget):
+    def __init__(self, taskName: str):
+        super().__init__()
+        self.taskName = taskName
+
+        self.taskLayout = QtWidgets.QHBoxLayout()
+
+        self.checkbox = QtWidgets.QCheckBox(taskName)
+        self.checkbox.setFixedHeight(30)
+        self.checkbox.setStyleSheet('''
+            background-color: rgb(83, 83, 83);
+        ''')
+        self.taskLayout.addWidget(self.checkbox)
+
+        self.addDeadlineBtn = QtWidgets.QPushButton("Добавить дедлайн")
+        self.addDeadlineBtn.setFixedWidth(120)
+        self.addDeadlineBtn.setFixedHeight(30)
+        self.addDeadlineBtn.setStyleSheet('''
+            background-color: rgb(83, 83, 83);
+        ''')
+        self.taskLayout.addWidget(self.addDeadlineBtn)
+
+        self.setLayout(self.taskLayout)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = ProjectWindow()
+    ex = ProjectWindow("New Project")
     ex.show()
     sys.exit(app.exec())
