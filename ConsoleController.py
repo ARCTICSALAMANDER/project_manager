6966056fa58b10925feb3d193ebb9d763a22875e
ARCTIC_SALAMANDER <ruslanau365@gmail.com>
2025-10-25 00:38:53 +0300

    Added interaction with Git to avoid access restrictions when binding a folder

--- ConsoleController.py
+++ ConsoleController.py
@@ -1,8 +1,12 @@
+import subprocess, os, shutil
+
+
 class Console():
     def __init__(self, projectWindow):
         self.projectWindow = projectWindow
         self.commands = ['help', 'add_task', 'delete_task']
         self.helpMessage = "PROJECT'S CONSOLE COMMANDS\nadd_task <task name>: adds a task to the task list\ndelete_task <task number>: deletes a task from the task list. Tasks' numeration starts from the top"
+        self.gitManager = GitManager(projectWindow)
 
     def commandExecuter(self):
         command = self.projectWindow.consoleInput.text()[2:]
@@ -28,13 +32,62 @@ class Console():
             else:
                 self.projectWindow.consoleOutput.setPlainText(f"Successfully deleted task №{taskNumber} from the task list")
         elif command[:space_index] == 'bind_folder':
-            projectFolderPath = command[space_index + 1:]
-            message, result = self.projectWindow.bindFolder(projectFolderPath)
-            if result:
-                self.projectFolderPath = projectFolderPath
-            else:
-                self.projectWindow.consoleOutput.setPlainText(f"FOLDER BINDING ERROR:\n{message}")
+            status = self.bindFolder(command, space_index)
+            if not status:
+                return
         else:
             self.projectWindow.consoleOutput.setPlainText(f"SYNTAX ERROR:\nthere is no command '{command[:space_index]}'")
 
-# "C:\Users\rusla\OneDrive\Desktop\LMS projects\PyQt project"
+    def bindFolder(self, command: str, space_index: int) -> bool:
+        '''Метод привязки папки через Git'''
+        projectFolderPath = command[space_index + 1:]
+        status = self.gitManager.bindGit()
+        if not status:
+            self.projectWindow.consoleOutput.setPlainText(f"ERROR:\nGit not found. Check if it had been added to PATH")
+        
+        status = self.gitManager.bindFolder(projectFolderPath)
+        if status:
+            self.projectWindow.consoleOutput.setPlainText("Folder binded successfully!")
+            return True
+        else:
+            return False
+
+class GitManager:
+    '''Управляет операциями с Git'''
+    def __init__(self, projectWindow):
+        self.projectWindow = projectWindow
+        self.gitPath = ""
+        self.projectFolder = None
+        self.isRepos = False
+
+    def bindGit(self) -> bool:
+        '''Привязка терминала Git через PATH.'''
+        gitPath = shutil.which("git")
+        if not gitPath:
+            return False
+        else:
+            self.gitPath = gitPath
+            return True
+
+    def bindFolder(self, folderPath: str) -> bool:
+        '''Привязка папки. Вернет False  в том случае, если папки не существует. Привяжет репозиторий, если он есть.'''
+        folderPath = os.path.normpath(folderPath)
+        if folderPath[0] == '"': # убираем кавычки, если они есть
+            folderPath = folderPath[1:]
+
+        if folderPath[-1] == '"':
+            folderPath = folderPath[:-1]
+
+        status = subprocess.run([self.gitPath, '-C', folderPath, 'status'], capture_output=True, text=True)
+        if status.returncode != 0: # status будет равен 0 в том случае, если папка существует и в ней есть репозиторий
+            errorText = status.stderr.lower() # смотрим текст ошибки
+            if "not a git repository" in errorText: # если папка есть, но в ней нет репозитория
+                self.projectWindow.consoleOutput.setPlainText("Folder binded successfully!")
+                return True
+            else: # если папки вообще нет
+                self.projectWindow.consoleOutput.setPlainText(f"FOLDER BINDING ERROR:\n{errorText}")
+                return False
+        else:
+            self.isRepos = True
+            return True
+        
